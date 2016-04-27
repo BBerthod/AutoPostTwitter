876fbf4fbbd8675e50818b9d5f71a87c4f4b0752
unknown <billy.berthod@gmail.com>
2016-04-27 14:06:51 +0200

    V0.2 post tweet ok

--- autoposttwitter.php
+++ autoposttwitter.php
@@ -123,33 +123,24 @@ class AutoPostTwitter extends Module
 
 }
 
-
 require_once('TwitterAPIExchange.php');
-/** Set access tokens here - see: https://dev.twitter.com/apps/ **/
 $settings = array(
     'oauth_access_token' => "2696463349-meob2jcqrtsoBgWRnGTfaqNs4dzGj4Ri3D6pw2v",
     'oauth_access_token_secret' => "bY31sBZHkbsUnEDbmkFcnggVGjGsFPl9Su6CeN7oGu6ht",
     'consumer_key' => "DwaIrBNnUBOvOepa4i27gM7iQ",
     'consumer_secret' => "uUwEnur8B0KMk3llUhKSXdBdKcLWzOxnGpUUJn7mJiGPjjgFfd"
 );
-$url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
-$requestMethod = "GET";
-if (isset($_GET['user']))  {$user = $_GET['user'];}  else {$user  = "iagdotme";}
-if (isset($_GET['count'])) {$count = $_GET['count'];} else {$count = 20;}
-$getfield = "?screen_name=$user&count=$count";
+
+$url = 'https://api.twitter.com/1.1/statuses/update.json'; 
+$requestMethod = 'POST';
+$postfields = array(
+    'status' => 'Hello world, bitch' ); 
 $twitter = new TwitterAPIExchange($settings);
-$string = json_decode($twitter->setGetfield($getfield)
-->buildOauth($url, $requestMethod)
-->performRequest(),$assoc = TRUE);
-if($string["errors"][0]["message"] != "") {echo "<h3>Sorry, there was a problem.</h3><p>Twitter returned the following error message:</p><p><em>".$string[errors][0]["message"]."</em></p>";exit();}
-foreach($string as $items)
-    {
-        echo "Time and Date of Tweet: ".$items['created_at']."<br />";
-        echo "Tweet: ". $items['text']."<br />";
-        echo "Tweeted by: ". $items['user']['name']."<br />";
-        echo "Screen name: ". $items['user']['screen_name']."<br />";
-        echo "Followers: ". $items['user']['followers_count']."<br />";
-        echo "Friends: ". $items['user']['friends_count']."<br />";
-        echo "Listed: ". $items['user']['listed_count']."<br /><hr />";
-    }
+
+echo $twitter->buildOauth($url, $requestMethod)
+             ->setPostfields($postfields)
+             ->performRequest();
+
+             
+
 ?>
